8302b8018a0ed9af790fde1626aabdbd5d1e8c2d
DESKTOP-3A7GEQ6\Piotrek <piotr.jakubowski001@gmail.com>
2020-11-15 13:23:31 +0100

    feat: change hero package and add navigation script

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class Unit : MonoBehaviour
+{
+    NavMeshAgent nav;
+    public Transform targetToFollow;
+    Animator animator;
+
+    const string ANIMATOR_SPEED = "Speed",
+        ANIMATOR_DIE = "Die",
+        ANIMATOR_ATTACK = "Basic Attack";
+     
+    void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if(targetToFollow)
+        {
+            nav.SetDestination(targetToFollow.position);
+        }
+        Animate();
+    }
+    protected virtual void Animate()
+    {
+        var speedVector = nav.velocity;
+        speedVector.y = 0;
+        float speed = speedVector.magnitude;
+        animator.SetFloat(ANIMATOR_SPEED, speed);
+
+    }
+}
