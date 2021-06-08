1b8add1e65be1de5701a586c299032819c30ba7a
DESKTOP-8STL7JV\Piotrek <piotr.jakubowski001@gmail.com>
2021-06-08 22:46:54 +0200

    i switched folder name and added testing zenject monoinstaller

---
+++ Assets/_Scripts/Units/Concrete/Test.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class Test : ITest, IInitializable
+{
+    public void Initialize()
+    {
+        Debug.Log("Hi zenjact!!");
+    }
+}
